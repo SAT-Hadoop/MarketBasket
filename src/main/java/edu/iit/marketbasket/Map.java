3acf08bb16e6fed537afa45b1e58ea956058d33a
saipramod <supadyay@hawk.iit.edu>
2015-05-01 13:47:59 -0500

    updated the pom

--- src/main/java/edu/iit/marketbasket/Map.java
+++ src/main/java/edu/iit/marketbasket/Map.java
@@ -12,12 +12,15 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapreduce.MapContext;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
@@ -29,6 +32,15 @@ public class Map extends MapReduceBase implements Mapper {
 
     private static final Logger sLogger = Logger.getLogger(Map.class);
 
+    private String mapTaskId;
+    private String inputFile;
+
+    @Override
+    public void configure(JobConf job) {
+        mapTaskId = job.get("mapred.task.id");
+        inputFile = job.get("map.input.file");
+    }
+
     @Override
     public void map(Object k1, Object v1, OutputCollector oc, Reporter rprtr) throws IOException {
         IntWritable one = new IntWritable(1);
@@ -36,6 +48,7 @@ public class Map extends MapReduceBase implements Mapper {
         String line = v1.toString();
         StringTokenizer tokenizer = new StringTokenizer(line);
         List listOfWords = new ArrayList();
+
         while (tokenizer.hasMoreTokens()) {
             //listOfWords.add(tokenizer.nextToken());
             String[] wordsInALine = tokenizer.nextToken().split(",");
@@ -47,7 +60,7 @@ public class Map extends MapReduceBase implements Mapper {
             for (int i = 0; i < listOfWords.size() - 1; i++) {
                 for (int j = i + 1; j < listOfWords.size(); j++) {
                     pair.set(listOfWords.get(i) + "_" + listOfWords.get(j));
-                    sLogger.log(Level.INFO,"the words are " + listOfWords.get(i) + "_" + listOfWords.get(j));
+                    sLogger.log(Level.INFO, "the words are " + mapTaskId + "  "+ listOfWords.get(i) + "_" + listOfWords.get(j));
                     oc.collect(pair, one);
                 }
             }
